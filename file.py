ed837054d35acdd0d93073dba2c23c56b18d9631
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-03-26 09:32:05 +0100

    updated

--- file.py
+++ file.py
@@ -6,14 +6,11 @@ class Student:
     storing it in a file and providing functionalities to write new 
     student records to the file
     """
-
-    numberOfStudents = 1  # Class variable
-
-    def __init__(self, names, age, score):
+    def __init__(self, names, course, level):
         """Initialize a student object"""
         self.names = names
-        self.age = age
-        self.score = score
+        self.course = course
+        self.level = level
 
     def copyToFile(self):
         """Write student information to a file"""
@@ -22,24 +19,47 @@ class Student:
                 if self.names.lower() in line.lower():
                     print("Name already exists")
                     return  # Exit the method if the name already exists
-                else:
-                    Student.numberOfStudents += 1   # Increment numberOfStudents
         # If the name doesn't exist, append the student record to the file
         with open("file.txt", "a") as f:
-            f.write(f"({Student.numberOfStudents}) Name--> {self.names}  age--> {self.age}  score--> {self.score}\n")
+            f.write(f"Name--> {self.names}  course--> {self.course}  level--> {self.level}\n")
+            f.close()
+            print(f"Student {self.names} has been added to the file")
+
+    def removeFromFile(self):
+        """Remove student information from a file"""
+        with open("file.txt", "r") as f:
+            lines = f.readlines()
+
+        removed = False
+        with open("file.txt", "w") as f:
+            for line in lines:
+                if self.names.lower() not in line.lower():
+                    f.write(line)
+                else:
+                    removed = True
+        if removed:
+            print(f"Student {self.names} has been removed from the file")
+        else:
+            print(f"Student {self.names} not found in the file")
 
     @classmethod
     def readFromFile(cls):
         """Read student information from a file"""
         with open("file.txt", "r") as f:
             f_content = f.read()
-            print(f_content)
+            if f_content == "":
+                print("File is empty")
+            else:
+                print(f_content)
 
 # Create a student object
-newStudent = Student("Yusuf Oluwatobi", 25, 90.0)
+newStudent = Student("Yusuf Oluwatobi", "Mechanical Engineering", "100 Level")
 
 # Write student information to file
 newStudent.copyToFile()
+
+# Remove student information from file
+#newStudent.removeFromFile()
 
 # Read and print student information from file
 Student.readFromFile()
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-def sqrt(x):
-    return x ** 0.5
